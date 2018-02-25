8b51668fc0badcb11a80e6e4036f97566588a761
Rust86 <rustam_msu@mail.ru>
2018-02-25 13:13:52 +0300

    fixes plurals for models in addmin page

--- cs_weekends/website/models.py
+++ cs_weekends/website/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class  Session(models.Model):
+
+class Session(models.Model):
     weekend_num = models.IntegerField("weekend number",
-                                      help_text = "Number of a weekend that the session refers to.",
+                                      help_text="Number of a weekend that the session refers to.",
                                       )
     num = models.IntegerField("session number", unique=True)
     date = models.DateField(auto_now_add=False)
@@ -15,29 +16,34 @@ class  Session(models.Model):
     def __str__(self):
         return "Session #{}".format(self.num)
 
-class Page(models.Model):
 
+class Page(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(default=None)
 
-    #Timestamps
+    # Timestamps
     created_on = models.DateTimeField(auto_now_add=True)
     last_updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
 
+
 class Announcement(Page):
     pass
+
 
 class About(Page):
     pass
 
+
 class Supplementary(Page):
     pass
+    class Meta:
+        verbose_name_plural = "supplementaries"
+
 
 class Compendia(Page):
     pass
-
-
-
+    class Meta:
+        verbose_name_plural = "compendia"
