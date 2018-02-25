58d1d1bb08b0fd77c8cc3ce0f2d105fad6a7d65f
Rust86 <rustam_msu@mail.ru>
2018-02-25 16:04:19 +0300

    add mainpage.html

--- cs_weekends/website/views.py
+++ cs_weekends/website/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from .models import Session, Announcement, About, Compendia, Supplementary
 
 def default(request):
-    return render(request, "base.html")
+    return render(request, "mainpage.html")
 
 
